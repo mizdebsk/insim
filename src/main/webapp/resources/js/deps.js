275c68014a871a3ee70c7eae1b8edd299897b5c0
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-02-01 16:10:05 +0100

    Dependency graph diff

--- src/main/webapp/resources/js/deps.js
+++ src/main/webapp/resources/js/deps.js
@@ -27,3 +27,61 @@ function loadDependencyGraph(data) {
     }
     $("#jsviz").html(Viz('digraph {' + s + '}'));
 }
+
+function createDependencyGraphDiff(data1, data2) {
+    var s = '';
+
+    var data = {};
+
+    for ( var pkg in data1) {
+        if (data1.hasOwnProperty(pkg)) {
+            data[pkg] = data1[pkg];
+        }
+    }
+    for ( var pkg in data2) {
+        if (data2.hasOwnProperty(pkg)) {
+            if (data.hasOwnProperty(pkg)) {
+                var merged = data[pkg].concat(data2[pkg]).sort();
+                data[pkg] = merged.filter(function(item, pos) {
+                    return merged.indexOf(item) == pos
+                })
+            } else {
+                data[pkg] = data2[pkg];
+            }
+        }
+    }
+
+    for ( var pkg in data) {
+        if (data.hasOwnProperty(pkg)) {
+            s += '"' + pkg + '"';
+            if (!data1.hasOwnProperty(pkg)) {
+                s += "[color=green;fontcolor=green]";
+            } else if (!data2.hasOwnProperty(pkg)) {
+                s += "[color=red;fontcolor=red]";
+            }
+            s += ";";
+            deps = data[pkg];
+            deps.forEach(function(dep) {
+                s += '"' + pkg + '" -> "' + dep + '"';
+                if (!data1.hasOwnProperty(pkg) || !data1.hasOwnProperty(dep)
+                        || data1[pkg].indexOf(dep) == -1) {
+                    s += "[color=green]";
+                } else if (!data2.hasOwnProperty(pkg)
+                        || !data2.hasOwnProperty(dep)
+                        || data2[pkg].indexOf(dep) == -1) {
+                    s += "[color=red]";
+                }
+                s += ";";
+            });
+        }
+    }
+    $("#jsviz").html(Viz('digraph {' + s + '}'));
+}
+
+function loadDependencyGraphDiff(oldId, newId) {
+    $.getJSON("api/data/graph/" + oldId, function(oldData) {
+        $.getJSON("api/data/graph/" + newId, function(newData) {
+            createDependencyGraphDiff(oldData, newData);
+        });
+    });
+}
