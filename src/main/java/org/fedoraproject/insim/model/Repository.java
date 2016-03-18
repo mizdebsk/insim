80f53cbe6af51672f2c8ac75d03178d25daa7de3
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-03-18 08:56:40 +0100

    Add index on Repository.creationTime

--- src/main/java/org/fedoraproject/insim/model/Repository.java
+++ src/main/java/org/fedoraproject/insim/model/Repository.java
@@ -22,12 +22,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * @author Mikolaj Izdebski
  */
 @Entity
+@Table(indexes = { @Index(columnList = "creationTime") })
 public class Repository implements Serializable {
 
     private static final long serialVersionUID = 1;
